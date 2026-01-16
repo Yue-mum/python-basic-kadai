62778f33f63ca53c6f9c37ef9f095c62a418af3b
Yue-mum <tot.30320@gmail.com>
2026-01-16 11:13:11 +0900

    Replace console.log with print statements

--- kadai_007/kadai_007.py
+++ kadai_007/kadai_007.py
@@ -1,5 +1,5 @@
 array = ['月曜日は晴れです', '火曜日は雨です', '水曜日は晴れです', '木曜日は晴れです', '金曜日は曇りです', '土曜日は曇りのち雨です', '日曜日は雷雨です']
-console.log(array[2])
+print(array[2])
 dictionary = {'mon': '晴れ',
               'tue': '雨',
               'wed': '晴れ',
@@ -7,4 +7,4 @@ dictionary = {'mon': '晴れ',
               'fri': '曇り',
               'sat': '曇りのち雨',
               'sun': '雷雨'}
-              console.log(dictionary['wed'])
+print(dictionary['wed'])
